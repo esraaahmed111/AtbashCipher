59291a8ad7ba811f71ae20a8b474a3be270c4c90
esraaahmed111 <101480953+esraaahmed111@users.noreply.github.com>
2022-05-13 20:17:51 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -9,26 +9,7 @@ int main() {
         string message;
         getline(cin,message);
         bool flag = true ;
-        for( int i=0; i < message.size(); i++){
-            if(message[i] == ' '){
-                continue;
-            }
-            if (message[i]-'A'<0 || message[i]-'A'>26){
-            flag = false;
-            break;
-            }
-        }#include <iostream>
-using namespace std;
-string s;
-char letters[] = {'Z','Y','X','W','V','U','T','S','R','Q','P','O','N','M','L','K','J','I','H','G','F','E','D','C','B','A'}; //using frequency array.
-void atbash(string s);
-int main() {
-
-
-        string message;
-        getline(cin,message);
-        bool flag = true ;
-        for( int i=0; i < message.size(); i++){
+        for( int i=0; i < message.size(); ++i){
             if(message[i] == ' '){
                 continue;
             }
@@ -56,27 +37,4 @@ void atbash(string s) {
     }
 
 
-cout<<ans;
-}
-
-        if(flag == true) {
-            atbash(message);
-        }else cout << "invalid" << endl;
-
-
-    return 0;
-}
-
-void atbash(string s) {
-    string ans;
-    for (int i = 0; i < s.size(); ++i) {
-        if(s[i] == ' '){
-            ans += ' ' ;
-            continue;
-        }
-        ans += letters[s[i] - 'A'];
-    }
-
-
-cout<<ans;
-}
+cout << ans;
